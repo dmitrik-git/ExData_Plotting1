ff8df0b7a37df95a80d36886bea13606b9320887
Dmitri K <dmitri.kovagin@gmail.com>
2021-01-09 19:06:25 +0200

    added in-line comments

--- plot3.R
+++ plot3.R
@@ -19,20 +19,32 @@ dataOriginal <- cbind(dataOriginal, "DateTime" = strptime (paste (dataOriginal$D
 dataSet <- dataOriginal[as.Date(dataOriginal$Date, format = "%d/%m/%Y") == as.Date("2007-02-01"),]
 dataSet <- rbind(dataSet, dataOriginal[as.Date(dataOriginal$Date, format = "%d/%m/%Y") == as.Date("2007-02-02"),])
 
+# Setting up PNG device
 png(file = "plot3.png", width = 480, height = 480)
 
-dataSet <- transform (dataSet, Wday = factor(weekdays (dataSet$DateTime, abbreviate = TRUE) ))
+# yaxt to show the y-scaling, xaxt to make sure x-scale is visible 
+par (yaxt = 's', xaxt = 's') 
 
-
-par (yaxt = 's', yaxs = 'r', xaxt = 's')
+#First line
 with (dataSet, plot(DateTime, Sub_metering_1, ylab = "", type = "l"))
+
+# new to make lines overlap, yaxt to hide y-labels
 par(new = TRUE, yaxt = 'n')
+
+# Second line
 with (dataSet, plot(DateTime, Sub_metering_2, ylab = "", type = "l", col = "red", ylim = c(0,30)))
+
+# new to make lines overlap, yaxt to hide y-labels
 par(new = TRUE, yaxt = 'n')
+
+# Third line
 with (dataSet, plot(DateTime, Sub_metering_3, ylab= "Energy sub metering", type = "l", col = "blue", ylim = c(0,30)))
 
+# Adding the legend
 legend ("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-par(new = FALSE, yaxt = 's')
+
+# Resetting parameters
+par(new = FALSE, yaxt = 's', xaxt = 's')
 
 dev.off()
 
